1734eb4360a007577d0cd0ff83824fb553027d69
Jeremy Bacca Barquero <jbaccab@ucenfotec.ac.cr>
2024-01-11 18:27:51 -0600

    ADD NEW SERVICE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/views/home/home.component';
 import { NotfoundComponent } from './core/views/notfound/notfound.component';
+import { ApiService } from '@services/api.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,12 @@ import { NotfoundComponent } from './core/views/notfound/notfound.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
